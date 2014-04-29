428b8c174a20df5d2f9f85900fd056c10a62d0a1
Andy Ai <yanbo.ai@gmail.com>
2014-04-29 16:56:26 +0800

    Success response

--- src/test/java/org/jmotor/restful/AppTest.java
+++ src/test/java/org/jmotor/restful/AppTest.java
@@ -1,6 +1,5 @@
 package org.jmotor.restful;
 
-import org.jmotor.restful.response.Success;
 import org.junit.Test;
 
 /**
@@ -9,6 +8,5 @@ import org.junit.Test;
 public class AppTest {
     @Test
     public void test() {
-        Success.Created.build("https://server.com/v1/", "domain", "0001");
     }
 }
