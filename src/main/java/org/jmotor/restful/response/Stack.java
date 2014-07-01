757ab7e0d07749315e4a9adfe65718f4335b80f9
Andy Ai <yanbo.ai@gmail.com>
2014-07-01 16:32:02 +0800

    Details

--- src/main/java/org/jmotor/restful/response/Stack.java
+++ src/main/java/org/jmotor/restful/response/Stack.java
@@ -9,7 +9,10 @@ package org.jmotor.restful.response;
  */
 public class Stack {
     private String code;
+    private String filed;
     private String message;
+    private String resource;
+    private String document;
 
     public String getCode() {
         return code;
@@ -19,11 +22,35 @@ public class Stack {
         this.code = code;
     }
 
+    public String getFiled() {
+        return filed;
+    }
+
+    public void setFiled(String filed) {
+        this.filed = filed;
+    }
+
     public String getMessage() {
         return message;
     }
 
     public void setMessage(String message) {
         this.message = message;
+    }
+
+    public String getResource() {
+        return resource;
+    }
+
+    public void setResource(String resource) {
+        this.resource = resource;
+    }
+
+    public String getDocument() {
+        return document;
+    }
+
+    public void setDocument(String document) {
+        this.document = document;
     }
 }
